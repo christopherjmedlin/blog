8c7422edd127ad5a29a80be9f676ebdf92ca89cc
Christopher Medlin <cjmedlin99@yahoo.com>
2018-02-24 13:23:09 -0800

    Add more utility function tests

--- blog/tests.py
+++ blog/tests.py
@@ -16,13 +16,25 @@ def app_test(f):
 
 def test_sanitize_html():
     html_one = "<p></p><script>hackUser();</script><strong></strong>"
-    expected = "<p></p>hackUser();<strong></strong>"
-    assert utils.sanitize_html(html_one) == expected
-
     html_two = ('<h1>Title</h1><p>Hello</p>'
                 '<code>print("Hello World!")</code>')
+    expected_one = "<p></p>hackUser();<strong></strong>"
+
+    assert utils.sanitize_html(html_one) == expected_one
     assert utils.sanitize_html(html_two) == html_two
 
 def test_json_error():
     expected = '{"error": {"code": "404", "message": "Not found"}}'
     assert utils.json_error("Not found", "404") == expected
+
+def test_html_preview():
+    html_one = '<h1>Title</h1> <p>Content</p>'
+    html_two = '<p>Text</p><img src="a link" />'
+    assert utils.html_preview(html_one) == "Title Content"
+    assert utils.html_preview(html_two) == "Text"
+
+def test_get_first_img():
+    html_one = '<h1>Title</h1> <img src="https://image.com/img.jpg" />'
+    html_two = '<p>This HTML has no image</p>'
+    assert utils.get_first_img(html_one) == "https://image.com/img.jpg"
+    assert utils.get_first_img(html_two) == None
