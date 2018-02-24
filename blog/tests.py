1860cc41aa09c018424cdc87eaf3636a095834f3
Christopher Medlin <cjmedlin99@yahoo.com>
2018-02-24 13:07:35 -0800

    Add tests for sanitize_html and json_error

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,28 @@
 from blog import app, mongo
+from . import utils
 
-def test():
-    assert True
+from functools import wraps
+
+def app_test(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        app_client = app.test_client()
+
+    return decorated_function
+
+#########################
+# Unit Tests
+#########################
+
+def test_sanitize_html():
+    html_one = "<p></p><script>hackUser();</script><strong></strong>"
+    expected = "<p></p>hackUser();<strong></strong>"
+    assert utils.sanitize_html(html_one) == expected
+
+    html_two = ('<h1>Title</h1><p>Hello</p>'
+                '<code>print("Hello World!")</code>')
+    assert utils.sanitize_html(html_two) == html_two
+
+def test_json_error():
+    expected = '{"error": {"code": "404", "message": "Not found"}}'
+    assert utils.json_error("Not found", "404") == expected
